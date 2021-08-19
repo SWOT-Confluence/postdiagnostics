a2124df50d28f432074a3661cbf33f7c2ffba3b9
Nikki Tebaldi <ntebaldi@umass.edu>
2021-08-19 15:24:44 -0400

    Updated both FLPE and MOI diagnostics ops
    - FLPE now writes output and fixed bug with stability_check
    - MOI data now include FLPE and MOI results

--- postdiagnostics.R
+++ postdiagnostics.R
@@ -7,9 +7,10 @@ source("output.R")
 #' file.
 #' 
 #' @param input_dir string path to directory that contains input data
+#' @param output_dir string path to directory to write output to
 #'
 #' @return dataframe of FLPE and SoS data
-run_flpe_diagnostics <- function(input_dir) {
+run_flpe_diagnostics <- function(input_dir, output_dir) {
   # INPUT  
   # args <- commandArgs(trailingOnly=TRUE)
   # reaches_json <- ifelse(is.null(args), "reaches.json", args[1])
@@ -23,7 +24,7 @@ run_flpe_diagnostics <- function(input_dir) {
   diag_data_flpe <- flpe_diagnostics(data$curr, data$prev, 2)    ## TODO decide what is appropriate tolerance
   
   # OUTPUT
-  # write_data_flpe(diag_data)
+  write_data_flpe(diag_data_flpe, reach_files$reach_id, output_dir)
 }
 
 #' Run Integrator diagnostics - not complete
@@ -34,7 +35,7 @@ run_flpe_diagnostics <- function(input_dir) {
 #' @param input_dir string path to directory that contains input data
 #'
 #' @return ?
-run_integrator_diagnostics <- function(input_dir) {
+run_moi_diagnostics <- function(input_dir) {
   # INPUT  
   # args <- commandArgs(trailingOnly=TRUE)
   # reaches_json <- ifelse(is.null(args), "reaches.json", args[1])
@@ -42,10 +43,11 @@ run_integrator_diagnostics <- function(input_dir) {
   # index <- strtoi(Sys.getenv("AWS_BATCH_JOB_ARRAY_INDEX")) + 1 ## TODO for container
   index <- 14
   reach_files <- get_input_data(reaches_json, input_dir, index)
-  data <- get_data_moi(reach_files$sos, reach_files$reach_id, input_dir)
+  flpe_data <- get_flpe_current(reach_files$reach_id, input_dir)
+  moi_data <- get_data_moi(reach_files$sos, reach_files$reach_id, input_dir)
   
   # PROCESSING
-  # diag_data_moi <- moi_diagnostics(data)
+  diag_data_moi <- moi_diagnostics(flpe_data, moi_data$curr, moi_data$prev)
   
   # OUTPUT
   # write_data_integrator(diag_data)
@@ -67,7 +69,7 @@ flpe_diagnostics <- function(current_discharge, previous_discharge, tolerance) {
   #go needlessly fancy with an explicit name check and a lapply statment
   
   # algo_names=c("geobam", "hivdi", "momma", "sad", "metroman")
-  algo_names=c("geobam", "momma", "metroman")
+  algo_names=c("geobam", "hivdi", "momma", "metroman")
   
   #realism check
   realism_check= function(current_discharge,algo_name){
@@ -91,9 +93,9 @@ flpe_diagnostics <- function(current_discharge, previous_discharge, tolerance) {
   #stability check  ## TODO ‘-’ only defined for equally-sized data frames 
   stability_check=function(current_discharge, previous_discharge, algo_name, tolerance){
     #pass a tolerance in percent change to determine how much change is too much
-    this_algo_q_now= select(current_discharge,paste0(algo_name,'_q')) %>%
-    dplyr::filter(date %in% previous_discharge$date)
-    this_algo_q_then= select(previous_discharge,paste0(algo_name,'_q'))
+    this_algo_q_now = select(current_discharge,paste0(algo_name,'_q')) %>%
+      dplyr::filter(current_discharge$date %in% previous_discharge$date)
+    this_algo_q_then = select(previous_discharge,paste0(algo_name,'_q'))
     
     stability_flag=0
     if (any(  abs(((this_algo_q_now- this_algo_q_then)/this_algo_q_now)*100) > tolerance,
@@ -108,14 +110,24 @@ flpe_diagnostics <- function(current_discharge, previous_discharge, tolerance) {
   # stability_flags= sapply(algo_names, stability_check, current_discharge=data, 
   #                         previous_discharge=data+100,tolerance=difference_tolerance)
   stability_flags= sapply(algo_names, stability_check, current_discharge=current_discharge, 
-                          previous_discharge=previous_discharge, tolerance=difference_tolerance)
+                          previous_discharge=previous_discharge, tolerance=tolerance)
+  
+  return(list(realism_flags=realism_flags, stability_flags=stability_flags))
 }
 
 #' Diagnostics on Integrator discharge data
 #'
-#' @param data ??
-#'
+#' @param flpe_discharge reach-level FLPE discharge dataframe
+#' @param current_integrator current integrator discharge dataframe
+#' @param previous_integrator previous integrator discharge dataframe
 #' @return ??
-moi_diagnostics <- function(flpe_discharge, integrator_discharge, last_integrator_discharge) {
-  ## Do Stuff
+moi_diagnostics <- function(flpe_discharge, current_integrator, previous_integrator) {
+  # Helpful info: 
+  # current_integrator and previous_integrator columns name meanings:
+  # "algoname_q" means integrated discharge
+  # "algoname_qmean_b" means discharge before integration
+  # "algoname_qmean_a" means discharge after integration
+  # "sos_qmin" and "sos_qmax" are SoS priors
+  
+  ## COLIN WRITE MOI DIAGNOSTICS HERE
 }
