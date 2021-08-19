1901737135146abdbce2bf8b3248ae8cad4fee4f
Nikki Tebaldi <ntebaldi@umass.edu>
2021-08-19 15:39:51 -0400

    Update data directories

--- run_postdiagnostics.R
+++ run_postdiagnostics.R
@@ -8,18 +8,19 @@ library(rjson)
 
 # RUN FLPE DIAGNOSTICS
 start <- Sys.time()
-# CHANGE ME #
 input_dir <- file.path("/home", "nikki", "Documents", "confluence", "workspace",
-                       "diagnostics", "post_data", fsep=.Platform$file.sep)    
-run_flpe_diagnostics(input_dir)
+                       "diagnostics", "post_data", "input", fsep=.Platform$file.sep)
+output_dir <- file.path("/home", "nikki", "Documents", "confluence", "workspace",
+                        "diagnostics", "post_data", "output", "flpe", fsep=.Platform$file.sep)
+run_flpe_diagnostics(input_dir, output_dir)
 end <- Sys.time()
 print(paste0("Execution time: ", end - start))
 
 # RUN INTEGRATOR DIAGNOSTICS
-start <- Sys.time()
-# CHANGE ME #
 input_dir <- file.path("/home", "nikki", "Documents", "confluence", "workspace",
-                       "diagnostics", "post_data", fsep=.Platform$file.sep)    
-run_integrator_diagnostics(input_dir)
+                       "diagnostics", "post_data", "input", fsep=.Platform$file.sep)
+output_dir <- file.path("/home", "nikki", "Documents", "confluence", "workspace",
+                        "diagnostics", "post_data", "output", "moi", fsep=.Platform$file.sep)   
+run_moi_diagnostics(input_dir, output_dir)
 end <- Sys.time()
 print(paste0("Execution time: ", end - start))
