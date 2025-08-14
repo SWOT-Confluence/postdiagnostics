50142f82af639b127d870d7243ca0bdc3796c06a
Nikki <17799906+nikki-t@users.noreply.github.com>
2025-08-14 09:07:38 -0400

    Update container image name

--- terraform/modules/postdiagnostics/confluence-postdiagnostics-flpe.tf
+++ terraform/modules/postdiagnostics/confluence-postdiagnostics-flpe.tf
@@ -7,7 +7,7 @@ resource "aws_batch_job_definition" "generate_batch_jd_postdiagnostics_flpe" {
   tags = { "job_definition" : "${var.prefix}-postdiagnostics-flpe" }
 
   container_properties = jsonencode({
-    image = "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-postd-flpe:${var.image_tag}"
+    image = "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-postdiagnostics-flpe:${var.image_tag}"
     executionRoleArn = var.iam_execution_role_arn
     jobRoleArn = var.iam_job_role_arn
     fargatePlatformConfiguration = {
--- terraform/modules/postdiagnostics/confluence-postdiagnostics-moi.tf
+++ terraform/modules/postdiagnostics/confluence-postdiagnostics-moi.tf
@@ -7,7 +7,7 @@ resource "aws_batch_job_definition" "generate_batch_jd_postdiagnostics_moi" {
   tags = { "job_definition" : "${var.prefix}-postdiagnostics-moi" }
 
   container_properties = jsonencode({
-    image = "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-postd-moi:${var.image_tag}"
+    image = "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-postdiagnostics-moi:${var.image_tag}"
     executionRoleArn = var.iam_execution_role_arn
     jobRoleArn = var.iam_job_role_arn
     fargatePlatformConfiguration = {
